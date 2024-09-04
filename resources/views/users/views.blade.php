816ebf034e7e18f0383ef8bfcbcff19c27770b62
RaihanHossan99 <raihan24696@gmail.com>
2024-09-04 23:36:07 +0600

    view page edit

--- resources/views/users/views.blade.php
+++ resources/views/users/views.blade.php
@@ -5,11 +5,11 @@
 
 @section('content')
 <div class="w-75 mx-auto">
-    <div class="mt-5 w-50 mx-auto">
-        <img class="md_width" style="border-radius: 100%" src="{{ url('Uploaded_Photo/'. $item->Image) }}" alt="This is a pizza">
-        <p class="text-center text-capitalize name_width">{{$item->Name}}</p>
+    <div class="mt-5 w-25 mx-auto">
+        <img class="md_width" style="border-radius: 100%" src="{{ url('Uploaded_Photo/'. $item->Image) }}" alt="{{$item->alt}}">
+        <h2 class="text-center text-capitalize name_width text-secondary">{{$item->Name}}</h2>
     </div>
-    <div>
+    <div class="mt-5">
         {{-- title --}}
         <div>
             <h1>{{$item->Title}}</h1>
